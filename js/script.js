b59f1599e78c02ee4ac6169cdad99774dba78220
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-24 18:32:55 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Nathan De Silva. All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Nathan De Silva
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+function myButtonClicked() {
+  // input
+  const numberGuessed = parseInt(document.getElementById('guess').value)
+
+  // process
+  if (numberGuessed == randomNumber) {
+
+    // output
+    document.getElementById('answer').innerHTML = 'You guessed the correct number! The Number Was ' + randomNumber + '!'
+  }
+
+  // process
+  if (numberGuessed != randomNumber) {
+
+    // output
+    document.getElementById('answer').innerHTML = 'You have guessed the wrong number!'
+  }
+}
